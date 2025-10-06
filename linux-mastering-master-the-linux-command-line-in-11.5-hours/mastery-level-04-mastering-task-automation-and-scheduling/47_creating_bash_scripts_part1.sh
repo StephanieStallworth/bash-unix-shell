29cd1a392353a82bba36cdbadebcb1ed7485e419
Stephanie Stallworth <scstallworth1@gmail.com>
2025-10-06 12:29:39 -0400

    Delete linux-mastering-master-the-linux-command-line-in-11.5-hours directory

--- linux-mastering-master-the-linux-command-line-in-11.5-hours/mastery-level-04-mastering-task-automation-and-scheduling/47_creating_bash_scripts_part1.sh
+++
@@ -1,7 +0,0 @@
-#################### our_script.sh ####################
-#!/bin/bash
-
-mkdir ~/Desktop/mg
-cd ~/Desktop/mg
-touch file{1..100}
-ls -lh ~/Desktop/mg > ~/Desktop/mg.log
--- linux-mastering-master-the-linux-command-line-in-11.5-hours/mastery-level-04-mastering-task-automation-and-scheduling/50_schedule_automation_using_cron_part2.sh
+++
@@ -1,8 +0,0 @@
-#################### backup.sh #################### 
-#!/bin/bash
-
-# Make sure it always save to Desktop 
-tar -czf ~/backups/backup.tar.gz ~/{Documents,Downloads,Desktop,Pictures,Videos} 2>/dev/null # "bit bucket" # updating Desktop to backups folder
-
-# Log date backup happened 
-date >> ~/backups/backups.log
